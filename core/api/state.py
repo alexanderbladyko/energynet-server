b1a7cff23a1e36de1794972f2c46051257bc3377
alexanderbladyko <sanechekz@mail.ru>
2017-05-18 00:49:11 +0300

    added state api test

--- core/api/state.py
+++ core/api/state.py
@@ -2,10 +2,14 @@ from flask_socketio import emit
 from flask_login import current_user
 
 from core.models import User
+from utils.redis import redis
 
 
 def get_state(data):
-    user = User.get_by_id(current_user.id)
+    user = User.get_by_id(redis, current_user.id, [
+        User.current_game_id,
+        User.current_lobby_id,
+    ])
 
     emit('state', {
         'inGame': bool(user.current_game_id),
